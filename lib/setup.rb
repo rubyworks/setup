bafc3a7111730e0ef4e396f6da4b0966171feb17
7rans <transfire@gmail.com>
2010-06-10 10:18:42 -0400

    access to profile metdata from lib

--- lib/setup.rb
+++ lib/setup.rb
@@ -1,3 +1,25 @@
+require 'yaml'
+
 module Setup
-  VERSION   = '5.1.0'
+  DIRECTORY = File.dirname(__FILE__) + '/setup'
+
+  PROFILE = YAML.load(File.new(DIRECTORY + '/profile.yml'))
+  verfile = YAML.load(File.new(DIRECTORY + '/version.yml'))
+
+  VERSION = verfile.values_at('major','minor','patch','state','build').compact.join('.')
+
+  #
+  def self.const_missing(name)
+    key = name.to_s.downcase
+    if verfile.key?(key)
+      verfile[key]
+    elsif profile.key?(key)
+      PROFILE[key]
+    else
+      super(name)
+    end
+  end
 end
+
+require 'setup/session'
+
