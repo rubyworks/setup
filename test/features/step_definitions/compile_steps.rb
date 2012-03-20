341883f3b2828b8bee016331ec8b63b07c8af4e3
7rans <transfire@gmail.com>
2012-03-20 14:07:57 -0400

    The setup phase was renamed to compile.

--- test/features/step_definitions/compile_steps.rb
+++ test/features/step_definitions/compile_steps.rb
@@ -6,13 +6,13 @@ When /^I issue the command 'setup\.rb compile'$/ do
   Setup::Command.run("compile", "--quiet")
 end
 
-When /^I issue the command 'setup\.rb install' unprepared$/ do
-  begin
-    Setup::Command.run("install", "--quiet")
-  rescue SystemExit => error
-    $setup_feature_error = error
-  end
-end
+#When /^I issue the command 'setup\.rb install' unprepared$/ do
+#  begin
+#    Setup::Command.run("install", "--quiet")
+#  rescue SystemExit => error
+#    $setup_feature_error = error
+#  end
+#end
 
 Then /^the extensions should be compiled$/ do
   exts = Dir['ext/faux/faux.so']
