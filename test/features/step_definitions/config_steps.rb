f125134acc60407fdeaa339b9cbf9e1800097db2
7rans <transfire@gmail.com>
2012-03-20 13:34:34 -0400

    Remove preconfig step definitions. [test[

--- test/features/step_definitions/config_steps.rb
+++ test/features/step_definitions/config_steps.rb
@@ -6,19 +6,19 @@ Given /^'setup.rb config' has been run$/ do
   Setup::Command.run("config", "--quiet") #, "--trace")
 end
 
-#Given /^'setup\.rb config \-\-type=(.*?)' has been run$/ do |type|
-#  Setup::Command.run("config", "--type", type, "--quiet")
-#end
+Given /^'setup\.rb config \-\-type=(.*?)' has been run$/ do |type|
+  Setup::Command.run("config", "--type", type, "--quiet")
+end
 
 When /^I issue the command 'setup.rb config'$/ do
   Setup::Command.run("config", "--quiet") #, "--trace")
 end
 
-#Then /^a config file should be generated$/ do
-#  File.assert.exists?(Setup::Configuration::CONFIG_FILE)
-#end
+Then /^a config file should be generated$/ do
+  File.assert.exists?(Setup::Configuration::CONFIG_FILE)
+end
 
-#Then /^the config file should be updated$/ do
-#  File.assert.exists?(Setup::Configuration::CONFIG_FILE)
-#end
+Then /^the config file should be updated$/ do
+  File.assert.exists?(Setup::Configuration::CONFIG_FILE)
+end
 
